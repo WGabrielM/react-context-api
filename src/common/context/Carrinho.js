1fb161abb4c33efacd689f80168b907a525a25b3
Wellington Gabriel Monteiro Batista <wellington.gabriel@metropoles.com>
2023-07-07 14:35:58 -0300

    adicionando comando para adicionar e remover itens do carrinho

--- src/common/context/Carrinho.js
+++ src/common/context/Carrinho.js
@@ -15,6 +15,13 @@ export const CarrinhoProvider = ({ children }) => {
 export const useCarrinhoContext = () => {
   const { carrinho, setCarrinho } = useContext(CarrinhoContext);
 
+  function mudarQuantidade(id, quantidade) {
+    return setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDoCarrinho => {
+      if(itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
+      return itemDoCarrinho;
+    }))
+  }
+
   function adicionarProduto(novoProduto) {
     const temOProduto = carrinho.some(itemDoCarrinho => itemDoCarrinho.id === novoProduto.id);
     if(!temOProduto){
@@ -25,11 +32,18 @@ export const useCarrinhoContext = () => {
       ]);
     }
 
-    setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDoCarrinho => {
-      if(itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
-      return itemDoCarrinho;
-    }))
+    setCarrinho(mudarQuantidade(novoProduto.id));
+  }
+  
+  function removerProduto(id) {
+    const produto = carrinho.find(itemDoCarrinho => itemDoCarrinho.id ===id);
+    const ehOUltimo = produto.quantidade === 1;
+    
+    if(ehOUltimo) {
+      setCarrinho(carrinhoAnterior => carrinhoAnterior.filter(itemDoCarrinho => itemDoCarrinho.id !== id))
+    }      
+    setCarrinho(mudarQuantidade(id));
   }
 
-  return carrinho, setCarrinho, adicionarProduto;
+  return carrinho, setCarrinho, adicionarProduto, removerProduto;
 };
